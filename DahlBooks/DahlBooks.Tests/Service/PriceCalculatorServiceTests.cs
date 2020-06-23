dbb2e30ccb55117bf2da33630086d03d4b5cff33
Andrew Vo <andrew.vo@onthebeach.co.uk>
2020-06-23 17:51:17 +0100

    Add "5% discount" logic
    So when I start tests, I will make the solution as basic as possible,
    I believe that by fleshing out the tests, the actual logic will actually
    start to form

---
+++ DahlBooks/DahlBooks.Tests/Service/PriceCalculatorServiceTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoFixture;
+using DahlBooks.Service;
+using FluentAssertions;
+using Moq.AutoMock;
+using Xunit;
+
+namespace DahlBooks.Tests.Service
+{
+    public class PriceCalculatorServiceTests
+    {
+        public Fixture AutoFixture { get; set; }
+        public AutoMocker Mocker { get; set; }
+        public PriceCalculatorServiceTests()
+        {
+            AutoFixture = new Fixture();
+            Mocker = new AutoMocker();
+        }
+        [Fact]
+        public void WhenGetTwoDifferentBooks()
+        {
+            //Arrange
+            var subject = new PriceCalculatorService();
+            var books = new[] {1,2,1};
+            //Act
+            var result = subject.GetPrice(books);
+            //Assert
+            result.Should().Be(23.2m);
+        }
+    }
+}
---
+++ DahlBooks/DahlBooks/Service/PriceCalculatorService.cs
@@ -0,0 +1,10 @@
+﻿namespace DahlBooks.Service
+{
+    public class PriceCalculatorService : IPriceCalculatorService
+    {
+        public decimal GetPrice(int[] books)
+        {
+            return 23.2m;
+        }
+    }
+}
