68cfbec75673ee8371366edda3bc8bb32372f004
Andrew Vo <andrew.vo@onthebeach.co.uk>
2020-06-23 17:22:56 +0100

    Create new project and test project

---
+++ DahlBooks/DahlBooks.Tests/Controllers/PriceCalculatorControllerTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoFixture;
+using DahlBooks.Controllers;
+using FluentAssertions;
+using Moq.AutoMock;
+using Xunit;
+
+namespace DahlBooks.Tests.Controllers
+{
+    public class PriceCalculatorControllerTests
+    {
+        public Fixture AutoFixture { get; set; }
+        public AutoMocker Mocker { get; set; }      
+        public PriceCalculatorControllerTests()
+        {
+            AutoFixture = new Fixture();
+            Mocker = new AutoMocker();
+        }
+
+        [Fact]
+        public void WhenGet()
+        {
+            //Arrange
+            var subject = Mocker.CreateInstance<PriceCalculatorController>();
+            var books = AutoFixture.Create<int[]>();
+
+            //Act
+            var result = subject.Get(books);
+
+            //Assert
+            result.Should().BeGreaterThan(0.00m);
+        }
+    }
+}
---
+++ DahlBooks/DahlBooks/Controllers/PriceCalculatorController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DahlBooks.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PriceCalculatorController : ControllerBase
+    {
+        [HttpGet]
+        public decimal Get([FromBody] int[] books)
+        {
+            return 0.00m;
+        }
+    }
+}
