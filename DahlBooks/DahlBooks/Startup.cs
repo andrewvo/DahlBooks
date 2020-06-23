656dda79f94bc9becdaf61634a73a5a603382b7b
Andrew Vo <andrew.vo@onthebeach.co.uk>
2020-06-24 00:09:46 +0100

    Remove template WeatherForecast class from project and add IoC transients

--- DahlBooks/DahlBooks/Startup.cs
+++ DahlBooks/DahlBooks/Startup.cs
@@ -2,6 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DahlBooks.Helpers;
+using DahlBooks.Service;
+using DahlBooks.Tests.Service;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -25,6 +28,10 @@ namespace DahlBooks
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IPriceCalculatorService, PriceCalculatorService>();
+            services.AddTransient<IGroupNumbersByOccurrences, GroupNumbersByOccurrences>();
+            services.AddTransient<ICalculateDiscountedPrice, CalculateDiscountedPrice>();
+            services.AddTransient<IGetBookDiscountCombinations, GetBookDiscountCombinations>();
             services.AddControllers();
         }
 
